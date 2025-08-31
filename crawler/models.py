d7b3a8ce5e0f8856a7e8d5b9df6919ad3a33bcf3
Alireza <alirzahosinopoor.1390@gmail.com>
2025-08-31 22:05:58 +0330

    Added Config model in crawler app

--- crawler/models.py
+++ crawler/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Config(models.Model):
+    protocol = models.CharField(max_length=50)
+    config_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.protocol} - {self.id}"
